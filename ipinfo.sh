2ef83c52967df76ecfa4bbbdb71527050338d345
Seven0492 <102933041+Seven0492@users.noreply.github.com>
2022-08-16 01:08:06 -0400

    Replaced bloated code with a snippet equivalent

--- ipinfo.sh
+++ ipinfo.sh
@@ -2,15 +2,7 @@
 
 declare -r en_tock="[INSERT_BASE64_ENCRYPTED_TOKEN]"
 
-declare -r token=("$(cd /tmp || cd /tmp || return; touch random_07.txt; echo "$en_tock" >> random_07.txt; base64 -d random_07.txt)")
-
-declare if_exist=("$(ls /tmp || grep "random_07.txt")")
-
-# shellcheck disable=SC2128
-if [ "$if_exist" != NULL ]; then
-    cd /tmp || cd /tmp || return; rm random_07.txt
-    unset if_exist
-fi
+declare -r token=("$(cd /tmp || cd /tmp || return; touch random_07.txt; echo "$en_tock" >> random_07.txt; base64 -d random_07.txt || return; rm random_07.txt)")
 
 # Quotes are messy, but don't worry, it works.
 # shellcheck disable=SC2128
