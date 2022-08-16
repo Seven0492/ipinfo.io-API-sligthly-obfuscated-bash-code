838e6e4b2dab8bae5026a4886c4a10ea60eccda6
Seven0492 <102933041+Seven0492@users.noreply.github.com>
2022-08-15 22:47:19 -0400

    Adding a debug argument to test and delay the querry.

--- ipinfo.sh
+++ ipinfo.sh
@@ -6,31 +6,45 @@ declare -r token=("$(cd /tmp || cd /tmp || return; touch random_07.txt; echo "$e
 
 declare if_exist=("$(ls /tmp || grep "random_07.txt")")
 
-# shellcheck disable=SC2128
-if [ "$if_exist" != NULL ] ; then
+if [ "$if_exist" != NULL ]; then
     cd /tmp || cd /tmp || return; rm random_07.txt
     unset if_exist
 fi
 
 # Quotes are messy, but don't worry, it works.
-# shellcheck disable=SC2128
 declare -r ipinfo_1=("$(sed 's/.$//' <<< "$(sed "s/'//" <<< "https://ipinfo.io?token='$token'";)" )")
 
-if [ "$1" == "" ] ; then
+if [ "$1" == "" ] || [ "$1" == "debug" ] ; then
 
-    # shellcheck disable=SC2128
-    if [ "$ipinfo_1" != NULL ] ; then
+    if [ "$1" == "debug" ]; then
+        echo "Debug ipinfo_1: {$ipinfo_1}"
+        echo "Waiting 60 seconds"
+        echo
+        sleep 60
+        curl "$ipinfo_1"
+        echo
+        exit
+    fi
+    if [ "$ipinfo_1" != NULL ]; then
         curl "$ipinfo_1"
         echo
     fi
     exit
 fi
 
-# shellcheck disable=SC2128
-declare -r ipinfo_2=("$(sed 's/.$//' <<< "$(sed "s/'//" <<< "https://ipinfo.io/{$1}?token='$token'";)" )")
+declare -r ipinfo_2=("$(sed 's/.$//' <<< "$(sed "s/'//" <<< "https://ipinfo.io/$1?token='$token'";)" )")
 
-# shellcheck disable=SC2128
-if [ "$ipinfo_2" != NULL ] ; then
+if [ "$2" == "debug" ]; then
+    echo "Debug ipinfo_2: {$ipinfo_2}"
+    echo "Waiting 60 seconds"
+    echo
+    sleep 60
+    curl "$ipinfo_2"
+    echo
+    exit
+fi
+
+if [ "$ipinfo_2" != NULL ]; then
     curl "$ipinfo_2"
     echo
 fi
