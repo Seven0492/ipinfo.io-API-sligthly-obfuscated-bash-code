eda62f9edb996f95706ca5182858934361f02af8
Seven0492 <102933041+Seven0492@users.noreply.github.com>
2022-08-16 18:10:53 -0400

    Update base64_encrypt.sh

--- base64_encrypt.sh
+++ base64_encrypt.sh
@@ -2,6 +2,8 @@
 
 if [ "$1" != "" ]; then
 	declare unencrypted="$1"
-	echo "Here is the base64 version: $(cd /tmp || cd /tmp || return; touch unencrypted_07.txt; "$unencrypted" >> unencrypted_07.txt; base64 unencrypted_07.txt || return; rm unencrypted_07.txt)"
-	unset unencrypted
+	declare encrypted=""
+	encrypted=$(cd /tmp || cd /tmp || return; touch unencrypted_07.txt; echo "$unencrypted" >> unencrypted_07.txt; base64 unencrypted_07.txt || return; rm unencrypted_07.txt)
+	echo "Here is the base64 version: {$encrypted}"
+	unset unencrypted; unset encrypted
 fi
